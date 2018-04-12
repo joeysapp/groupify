be6d54377a208049e2ea7bd0eb29fe33337ba18b
Joseph Sapp <sapp.joey@tamu.edu>
2018-04-11 21:18:48 -0500

    streamlining auth flow

--- server.js
+++ server.js
@@ -109,67 +109,66 @@ app.get('/login', (req, res) => {
 
 app.get('/authResponse', function(req, res) {
 
-  // your application requests refresh and access tokens
-  // after checking the state parameter
+	// your application requests refresh and access tokens
+	// after checking the state parameter
 
-  console.log('User said yes');
+	console.log('User said yes');
 
-  var stateKey = 'spotify-auth-state';
+	var stateKey = 'spotify-auth-state';
 
-  var code = req.query.code || null;
-  var state = req.query.state || null;
-  var storedState = req.cookies ? req.cookies[stateKey] : null;
+	var code = req.query.code || null;
+	var state = req.query.state || null;
+	var storedState = req.cookies ? req.cookies[stateKey] : null;
 
-  if (state === null || state !== storedState) {
-    res.redirect('/#' +
-      querystring.stringify({
-        error: 'state_mismatch'
-      }));
-  } else {
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
+	if (state === null || state !== storedState) {
+		res.redirect('/#' +
+		querystring.stringify({
+		error: 'state_mismatch'
+		}));
+	} else {
+		var authOptions = {
+			url: 'https://accounts.spotify.com/api/token',
+			form: {
+			code: code,
+			redirect_uri: redirect_uri,
+			grant_type: 'authorization_code'
+		},
+		headers: {
+			'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+		},
+		json: true
+	};
 
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
+	request.post(authOptions, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			var access_token = body.access_token,
+			refresh_token = body.refresh_token;
 
-        var access_token = body.access_token,
-            refresh_token = body.refresh_token;
+			var options = {
+				url: 'https://api.spotify.com/v1/me',
+				headers: { 'Authorization': 'Bearer ' + access_token },
+				json: true
+			};
+			// use the access token to access the Spotify Web API
+			request.get(options, function(error, response, body) {
+				console.log('u got here btw!');
+				console.log(body);
+			});
 
-        var options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
-
-        // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
-          console.log(body);
-        });
-
-        // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
-      } else {
-        res.redirect('/#' +
-          querystring.stringify({
-            error: 'invalid_token'
-          }));
-      }
-    });
-  }
+	// we can also pass the token to the browser to make requests from there
+			res.redirect('/#' +
+				querystring.stringify({
+					access_token: access_token,
+					refresh_token: refresh_token
+			}));
+		} else {
+			res.redirect('/#' +
+				querystring.stringify({
+					error: 'invalid_token'
+			}));
+		}
+	});
+	}
 });
 
 app.get('/getAuthURL', function(req, res){
@@ -180,12 +179,6 @@ app.get('/getAuthURL', function(req, res){
 	res.send(authorizeURL);
 });
 
-app.get('/callback', function(req, res){
-	console.log('get: \'/callback');
-	console.log(req.query);
-	// res.sendFile(path.join(__dirname + '/index.html'));
-})
-
 // Spotify stuff!
 var SpotifyWebApi = require('spotify-web-api-node');
 var spotifyApi = new SpotifyWebApi({
@@ -193,29 +186,4 @@ var spotifyApi = new SpotifyWebApi({
 	redirectUri : 'http://localhost:8000/authResponse/'
 });
 
-// var sqlite3 = require('sqlite3').verbose();
-// var db = new sqlite3.Database('data/users.db', (e) => {
-// 	console.log('Database opened');
-// });
-
-// db.run('CREATE TABLE users (uuid VARCHAR(36), username TEXT, auth_token TEXT, refresh_token INT)');
-
-
-// db.all('SELECT * FROM users;', function(err, row){
-// 	console.log(row);
-// })
-
-// db.close(function(){
-// 	console.log('Database closed');
-// })
-
-// This would be if I wanted to have things happening
-// to the users without any causation
-// i.e. no one else doing anything (perlin noise movement!)
-// setInterval(function foo(){
-// 	console.log('helo');
-
-// }, 10000);
-
-app.listen(8888);
 
